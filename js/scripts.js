872df400c854d2d53888f8221236c3478bdd86e0
Brian Thomas <btdeveloper125@gmail.com>
2021-06-16 08:08:13 -0600

    This commit adds 1st array of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
-alert("Hello World");
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Charmandoer',
+    height: 0.6,
+    types: [
+      'fire',
+      'speed'
+    ]
+  },
+  {
+    name: 'Pidgey',
+    height: 0.3,
+    types: [
+      'flying',
+      'normal'
+    ]
+  },
+  {
+    name: 'Nidoking',
+    height: 1.4,
+    types: [
+      'ground',
+      'poison'
+    ]
+  },
+  {
+    name: 'Weepinbell',
+    height: 1,
+    types: [
+      'grass',
+      'poison'
+    ]
+  },
+];
+console.log(pokemonList);
