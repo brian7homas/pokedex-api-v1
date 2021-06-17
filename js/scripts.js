0b724715f0a5c4edc6b4c5796839eaa710d0d202
Brian Thomas <btdeveloper125@gmail.com>
2021-06-17 11:42:39 -0600

    This commit adds part 2 of task-1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,39 @@
-//data
-let pokemonList = [
-  {
-    name: 'Charmandoer',
-    height: 0.6,
-    types: [
-      'fire',
-      'speed'
-    ]
-  },
-  {
-    name: 'Pidgey',
-    height: 0.3,
-    types: [
-      'flying',
-      'normal'
-    ]
-  },
-  {
-    name: 'Nidoking',
-    height: 1.4,
-    types: [
-      'ground',
-      'poison'
-    ]
-  },
-  {
-    name: 'Weepinbell',
-    height: 1,
-    types: [
-      'grass',
-      'poison'
-    ]
-  },
-];
-
-
-//define variables in the global scope 
-
-/*
-  reduce adds the current height(curr.height)
-  to the accelorator (acc)
-*/
-
+let pokemonRepository = (function(){
+  let pokemonList = [
+    {
+      name: 'Charmandoer',
+      height: 0.6,
+      types: [
+        'fire',
+        'speed'
+      ]
+    },
+    {
+      name: 'Pidgey',
+      height: 0.3,
+      types: [
+        'flying',
+        'normal'
+      ]
+    },
+    {
+      name: 'Nidoking',
+      height: 1.4,
+      types: [
+        'ground',
+        'poison'
+      ]
+    },
+    {
+      name: 'Weepinbell',
+      height: 1,
+      types: [
+        'grass',
+        'poison'
+      ]
+    },
+  ];
+  
 //add all height values
 let totalHeight = pokemonList.reduce((acc, curr)=> curr.height + acc, 0); //3.3
 //get number of object inside the array
@@ -50,13 +42,29 @@ let itemCount = pokemonList.length; //4
 //calculate the average height 
 let averageHeight = totalHeight/itemCount; //0.825
 
-//set a range of .2 ABOVE the average height
-let positiveRange = .2 + averageHeight //1.025
+// variable using this to be accessible in global context
+this.positiveRange = .2 + averageHeight; //1.025
+this.negitiveRange = averageHeight - .2;  //0.625
 
-//set a range of .2 BELOW the average height
-let negitiveRange = averageHeight - .2  //0.625
 
-pokemonList.forEach(element => {
+  function getAll(){
+    return pokemonList;
+  }
+  function add(item){
+    return pokemonList.push(item)
+  }
+  return{
+    add: add,
+    getAll: getAll
+  }
+})();
+
+console.log(pokemonRepository.add({name: 'Takka', height: 1.82}));
+console.log(pokemonRepository.getAll());
+
+// pokemonList is removed from global context
+// pokemonRepository.getAll() is how it can be accessed
+pokemonRepository.getAll().forEach(element => {
   if(element.height < negitiveRange){
     document.write(`${element.name} is below the set average height. <br>`)
   }else if(element.height > positiveRange){
