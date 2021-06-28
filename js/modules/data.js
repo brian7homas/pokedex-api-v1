f6f6901361d824be857ecd215a95d5568c247b91
Brian Thomas <btdeveloper125@gmail.com>
2021-06-28 11:38:46 -0600

    This commit exports what to interact with data

---
+++ js/modules/data.js
@@ -0,0 +1,3 @@
+let data = [{'id': '','imgUrl': '', 'name':'','height':'','weight':'','abilities':'','png':'','types':''}];
+let detailsKeys = Object.keys(data[0]);
+export default {data, detailsKeys};
