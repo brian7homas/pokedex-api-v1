0377e3c8949a20d95fe0d5a3d890c60e468e2249
Brian Thomas <btdeveloper125@gmail.com>
2021-06-30 08:35:14 -0600

    This commit reverses new file structure

--- js/modules/capitalize.js
+++
@@ -1,6 +0,0 @@
-function capitalize(str){
-  String(str)
-  let lower = str.toLowerCase();
-  return str.charAt(0).toUpperCase() + lower.slice(1)
-}
-export default capitalize
--- js/modules/data.js
+++
@@ -1,3 +0,0 @@
-let data = [{'id': '','imgUrl': '', 'name':'','height':'','weight':'','abilities':'','png':'','types':''}];
-let detailsKeys = Object.keys(data[0]);
-export default {data, detailsKeys};
